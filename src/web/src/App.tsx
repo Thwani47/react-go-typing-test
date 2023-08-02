9f8aa7fb0c082d804ab0400f6646d2a4c6bda8c7
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-02 22:39:06 +0200

    setup tailwindcss

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello to typing</h1>
+      <h1 className="mt-2 text-2xl font-bold">Hello to typing</h1>
+      <button>Click Me!</button>
     </>
   );
 }
