287432321e2d4ec7eb3382018788abace041d057
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-02 23:44:27 +0200

    setup navbar

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <div className="h-full min-h-full">
+      <NavBar />
       <h1 className="mt-2 text-2xl font-bold">Hello to typing</h1>
-      <button>Click Me!</button>
-    </>
+    </div>
   );
 }
 
