d63bcd51a4d0cb360882b4a751ddb6ff3716b15f
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-03 09:34:30 +0200

    setup favicon and fixed github icon twitter link

--- src/web/src/components/Footer.tsx
+++ src/web/src/components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
           </svg>
         </a>
 
-        <a href="https://github.com/Thwani47" target="_blank">
+        <a href="https://github.com/Thwani47/react-go-typing-test" target="_blank">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
