6a83d11d44b75c67856ecee5a3aa6afa9bc38ea6
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-03 09:55:05 +0200

    fixed theming colors on footer and nabar

--- src/web/src/components/NavBar.tsx
+++ src/web/src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
 export default function NavBar() {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("black");
 
   function toggleTheme() {
-    setTheme(theme === "dark" ? "light" : "dark");
+    setTheme(theme === "black" ? "cupcake" : "black");
   }
 
   useEffect(() => {
@@ -13,7 +13,7 @@ export default function NavBar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-lg font-bold md:text-xl lg:text-2xl">
+        <a className="btn btn-ghost normal-case text-lg font-bold md:text-xl lg:text-2xl text-black dark:text-white">
           Typing <span className="text-pink-700 text-xl font-bold md:text-2xl lg:text-3xl">Test</span>
         </a>
       </div>
--- src/web/tailwind.config.js
+++ src/web/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
   theme: {
     extend: {}
   },
   daisyui: {
-    themes: ["light", "dark"]
+    themes: ["cupcake", "black"]
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")]
+  darkMode : ['class', '[data-theme="black"]']
 };
