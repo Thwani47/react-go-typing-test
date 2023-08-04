f9c4f39e9730779647c6f19d254f5c9de8e93430
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-04 23:19:27 +0200

    setup TakeTest page

---
+++ src/web/src/pages/TakeTest.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function TakeTest() {
+  return (
+    <Layout>
+      <div>TakeTest</div>
+    </Layout>
+  );
+}
--- src/web/src/routes/router.tsx
+++ src/web/src/routes/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import TypingTips from "../pages/TypingTips";
+import TakeTest from "../pages/TakeTest";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/typing-test",
+    element: <TakeTest />
   },
   {
     path: "typing-tips",
