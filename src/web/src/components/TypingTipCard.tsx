b81eb7d7803fe3be6ed4e92b7646d125ca81b77a
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-06 22:52:02 +0200

    setup PrismJS

--- src/web/src/components/TypingTipCard.tsx
+++ src/web/src/components/TypingTipCard.tsx
@@ -5,8 +5,8 @@ type TypingTipCardProps = {
 };
 export default function TypingTipCard({ tip }: TypingTipCardProps) {
   return (
-    <div className="w-[50%] p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-black dark:border-gray-700 hover:scale-110 transition duration-300 ease-in-out">
-      <h5 className="mb-2 text-2xl font-bold text-gray-900 dark:text-pink-700">
+    <div className="w-[70%] md:w-[50%] p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-black dark:border-gray-700 hover:scale-110 transition duration-300 ease-in-out">
+      <h5 className="mb-2 text-xl md:text-2xl font-bold text-gray-900 dark:text-pink-700">
         {tip.title}
       </h5>
       <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
--- src/web/src/pages/TakeTest.tsx
+++ src/web/src/pages/TakeTest.tsx
@@ -1,9 +1,39 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+
+const code = `const App = props => {
+    return (
+      <div>
+        <h1> React App </h1>
+        <div>Awesome code</div>
+      </div>
+    );
+  };
+  `;
 
 export default function TakeTest() {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
   return (
     <Layout>
-      <div>TakeTest</div>
+      <div>
+        <select className="select w-full max-w-xs">
+          <option disabled selected>
+            Pick your favorite Simpson
+          </option>
+          <option>Homer</option>
+          <option>Marge</option>
+          <option>Bart</option>
+          <option>Lisa</option>
+          <option>Maggie</option>
+        </select>
+        <pre>
+          <code className={`language-javascript`}>{code}</code>
+        </pre>
+      </div>
     </Layout>
   );
 }
